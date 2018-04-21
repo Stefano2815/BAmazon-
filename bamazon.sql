924ed6a27298ebbdd1066e4f69e4347af29e5b82
Falanga <stefano.falanga94@gmail.com>
2018-04-21 09:35:02 -0400

    ..

---
+++ bamazon.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE BAMAZON;
+USE BAMAZON;
+CREATE TABLE PRODUCTS (Auto_INCREMENT,
+id  INT Not NULL,
+productName VARCHAR (100),
+departmentName VARCHAR (100),
+stockQuantity VARCHAR (100),
+price VARCHAR (100),
+);
